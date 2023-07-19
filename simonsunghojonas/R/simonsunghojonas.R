515bab0298a15e3fc11b50df45d8f464c3b6f7cf
Sungho Park <park@cip.ifi.lmu.de>
2023-07-19 23:38:16 +0200

    added a function for count_characters

--- simonsunghojonas/R/simonsunghojonas.R
+++ simonsunghojonas/R/simonsunghojonas.R
@@ -2,13 +2,13 @@
 library(devtools)
 library(roxygen2)
   
-#' palindrome of String value
+#' palindrome of String value.
 #'
 #' This function takes an parameter representing the string 
-#' for which you want to create a palindrome.
-#' and makes input String value as a palindrome
-#' @param input_string String value,
-#' @return a String value , which is palindrome 
+#' for which you want to create a palindrome
+#' and makes input String value as a palindrome.
+#' @param input_string String value.
+#' @return a String value , which is palindrome.
 #' @examples
 #' input <- "hello"
 #' palindrome <- create_palindrome(input)
@@ -20,15 +20,15 @@ create_palindrome <- function(input_string) {
   return(palindrome)
 }
 
-#' swaps the symbols of two input indices in a string,
+#' swaps the symbols of two input indices in a string.
 #'
 #' This function swaps the symbols of two input indices in a string and
-#' returns the string with the swapped symbols 
+#' returns the string with the swapped symbols.
 
-#' @param input_string the string in which symbols need to be swapped
-#' @param index1 the first index
-#' @param index2 the second index
-#' @return string with the swapped symbols
+#' @param input_string the string in which symbols need to be swapped.
+#' @param index1 the first index.
+#' @param index2 the second index.
+#' @return string with the swapped symbols.
 #' @examples 
 #' input <- "abcdef"
 #' index1 <- 2
@@ -51,4 +51,27 @@ swap_symbols <- function(input_string, index1, index2) {
   return(swapped_string)
 }
 
+#' counts the number of characters in a string and determines whether it is an even or odd number
+#'
+#' This function takes an input string and counts the number of characters 
+#' in the string. It then determines whether the count is an even or odd number 
+#' and returns a descriptive string indicating the count and its parity.
 
+
+#' @param input_string The string for which you want to count the characters.
+#' @return A string indicating the number of characters in the input string and whether it is an even or odd count.
+#' @examples 
+#' input <- "Hello, World!"
+#' result <- count_characters(input)
+#' print(result)  # Output: "14 characters (even)"
+
+count_characters <- function(input_string) {
+  num_characters <- nchar(input_string)
+  is_even <- num_characters %% 2 == 0
+  
+  if (is_even) {
+    return(paste(num_characters, "characters (even)"))
+  } else {
+    return(paste(num_characters, "characters (odd)"))
+  }
+}
